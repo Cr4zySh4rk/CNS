cc4b3b7f9ab534bee18dcd77700b404c2c667110
Adithya Manjunath <adithya@Adithyas-MacBook-Air-2.local>
2023-01-19 20:49:38 +0530

    fixed LabProg12

--- LabProg12/lb.java
+++ LabProg12/lb.java
@@ -1,42 +1,65 @@
 import java.util.*;
-import java.io.*;
 public class lb
-{
-static int min(int x,int y)
-{
-if(x<y)
-return x;
-return y;
-}
-public static void main(String args[])
-{
-Scanner in = new Scanner(System.in);
-System.out.print("Enter the buckit size : ");
-int cap=in.nextInt();
-System.out.print("Enter rate : ");
-int rate=in.nextInt();
-int inp[]=new int[25];
-System.out.print("Enter number of seconds to simulate : ");
-int count=0,drop=0;
-int n=in.nextInt();
-for(int i=0;i<n;i++)
-{
-System.out.print("Enter size of packet at "+(i+1)+"s : ");
-inp[i]=in.nextInt();
-}
-int mini;
-System.out.println("Second\tRecvd\tSent\tRemaining\tDropped");
-System.out.println("-----------------------------------------");
-for(int i=0;i<n;i++)
-{
-count+=inp[i];
-if(count>cap)
-{
-drop=count-cap;
-count=cap;
-}
-mini=min(count,cap);
-System.out.println((i+1)+"\t"+inp[i]+"\t"+mini+"\t"+(cap-count)+"\t\t"+drop);
-}
-}
-}
+{ 
+    static int min(int x,int y) 
+    { 
+        if(x<y) 
+        return x; 
+        else 
+        return y; 
+    } 
+    public static void main(String[] args) 
+    { 
+        int drop=0,mini,nsec,cap,count=0,i,process; 
+        int inp[]=new int[25]; 
+        Scanner in=new Scanner(System.in); 
+        System.out.print("Enter The Bucket Size: "); 
+        cap= in.nextInt(); 
+        System.out.print("Enter The Operation Rate: "); 
+        process= in.nextInt(); 
+        System.out.print("Enter The No. Of Seconds You Want To Stimulate: "); 
+        nsec=in.nextInt(); 
+        for(i=0;i<nsec;i++) 
+        { 
+            System.out.print("Enter The Size Of The Packet Entering At "+ (i+1)+" sec: "); 
+            inp[i] = in.nextInt(); 
+        } 
+        System.out.println("\nSecond | Packet Recieved | Packet Sent | Packet Left | Packet Dropped|\n"); 
+        for(i=0;i<nsec;i++) 
+        { 
+            count+=inp[i]; 
+            if(count>cap) 
+            { 
+                drop=count-cap; 
+                count=cap; 
+            } 
+            System.out.print(i+1); 
+            System.out.print("\t\t"+inp[i]); 
+            mini=min(count,process); 
+            System.out.print("\t\t"+mini); 
+            count=count-mini; 
+            System.out.print("\t\t"+count); 
+            System.out.print("\t\t"+drop);
+            drop=0; 
+            System.out.println(); 
+        } 
+        for(;count!=0;i++) 
+        { 
+            if(count>cap) 
+            { 
+                drop=count-cap; 
+                count=cap; 
+            }    
+            System.out.print(i+1); 
+            System.out.print("\t\t0"); 
+            mini=min(count,process); 
+            System.out.print("\t\t"+mini); 
+            count=count-mini; 
+            System.out.print("\t\t"+count); 
+            System.out.print("\t\t"+drop); 
+            System.out.println(); 
+        } 
+    } 
+}   
+
+
